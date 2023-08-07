798be5cb6b1701c0494a2884553ae4ae0ebaf98f
akadwa <kadazh123@gmail.com>
2023-08-07 12:31:54 +0200

    Complete bubble-sort problem

---
+++ basic-ruby-projects/bubble-sort.rb
@@ -0,0 +1,21 @@
+def bubble_sort(array)
+  length = array.length
+
+  loop do
+    swapped = false
+
+    (length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1]=array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
+# [0,2,2,3,4,78]
